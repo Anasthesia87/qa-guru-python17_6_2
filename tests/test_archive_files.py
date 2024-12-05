d6e27ab7417b7818685f4a695fb128f6ef8287b7
Anastasiya_Lavretskaya <a.lavretskaya87@yandex.ru>
2024-12-05 22:07:48 +0300

    add edit test_archive_files.py

--- tests/test_archive_files.py
+++ tests/test_archive_files.py
@@ -1,4 +1,5 @@
 import csv
+from io import StringIO, BytesIO
 from pypdf import PdfReader
 from openpyxl import load_workbook
 import zipfile
@@ -6,42 +7,44 @@ from files_paths import *
 
 
 def test_csv_to_zip(create_archive):
-    with zipfile.ZipFile(ARCHIVE_PATH) as u_f:
-        csv_archived = u_f.extract('user_file.csv')
-        with open(csv_archived) as user_file:
-            csv_rows = csv.reader(user_file)
+    with zipfile.ZipFile(ARCHIVE_PATH) as csv_file:
+        with csv_file.open('user_file.csv') as file_example_csv:
+            content = file_example_csv.read().decode('utf-8')
+
+            file_like_object = StringIO(content)
+            csv_rows = csv.reader(file_like_object)
             new_list = []
             for row in csv_rows:
                 new_list.append(row)
+
             assert len(new_list) == 3, "Количество строк в выбранном файле не соответствует ожидаемому."
             assert new_list == [['1', 'alice', 'alice@example.com'],
                                 ['2', 'bob', 'bob@example.com'],
-                                ['3', 'carol', 'carol@example.com']], "Содержание выбранного файла не совпадает с ожидаемым."
-        os.remove('user_file.csv')
-
-        assert os.path.exists(ARCHIVE_PATH), "Архив был удален до окончания теста."
+                                ['3', 'carol',
+                                 'carol@example.com']], "Содержание выбранного файла не совпадает с ожидаемым."
 
 
 def test_pdf_to_zip(create_archive):
-    with zipfile.ZipFile(ARCHIVE_PATH) as p_f:
-        pdf_archived = p_f.extract('Python Testing with Pytest (Brian Okken).pdf')
-        reader = PdfReader(pdf_archived)
+    with zipfile.ZipFile(ARCHIVE_PATH) as pdf_file:
+        with pdf_file.open('Python Testing with Pytest (Brian Okken).pdf') as file_example_pdf:
+            content = BytesIO(file_example_pdf.read())
+
+        reader = PdfReader(content)
         assert len(reader.pages) == 256, "Количество страниц в выбранном файле не соответствует ожидаемому."
         page = reader.pages[1]
         text = page.extract_text()
         print(text)
-        assert 'Copyright © 2017 The Pragmatic Programmers, LLC' in text, "Содержание выбранного файла не совпадает с ожидаемым."
-        os.remove('Python Testing with Pytest (Brian Okken).pdf')
 
-        assert os.path.exists(ARCHIVE_PATH), "Архив был удален до окончания теста."
+        assert 'Copyright © 2017 The Pragmatic Programmers, LLC' in text, "Содержание выбранного файла не совпадает с ожидаемым."
 
 
 def test_xlsx_to_zip(create_archive):
-    with zipfile.ZipFile(ARCHIVE_PATH) as x_f:
-        xlsx_archived = x_f.extract('file_example_XLSX_50.xlsx')
-        workbook = load_workbook(xlsx_archived)
-        sheet = workbook.active
-        assert sheet.cell(row=49, column=2).value == 'Demetria', "Значение в указанной ячейке не соответствует ожидаемому."
-        os.remove('file_example_XLSX_50.xlsx')
+    with zipfile.ZipFile(ARCHIVE_PATH) as xlsx_file:
+        with xlsx_file.open('file_example_XLSX_50.xlsx') as file_example_xlsx:
+            content = BytesIO(file_example_xlsx.read())
 
-        assert os.path.exists(ARCHIVE_PATH),"Архив был удален до окончания теста."
+        workbook = load_workbook(filename=content)
+        sheet = workbook.active
+
+        assert sheet.cell(row=49,
+                          column=2).value == 'Demetria', "Значение в указанной ячейке не соответствует ожидаемому."
